1700e63d830ef378e979d68cfda9b79a9f8bbe93
LuizGabe <luizgabrielgrupe@gmail.com>
2022-11-17 21:46:06 -0300

    Switching to typescripts

---
+++ atividade01/src/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'App.sass'
+
+function App() {
+  // Aqui fica a configuração de pessoas que fazem parte do grupo
+  const pessoas = [
+    {nome:'Luiz Gabriel Grumicker Pereira', imgUrl: ''},
+    {nome:'Matheus Henrique da Rosa Foliatti', imgUrl: ''},
+    {nome:'Carlos Sadi Mucha Zomer', imgUrl: ''}
+  ]
+
+  return (
+    <div className='tudo'>
+
+      {/* Aqui fica o Titulo */}
+      <h1 id='titulo'>Integrantes</h1>
+
+      {/* Aqui começa a div com as pessoas (imagem e nome) EM GRUPO */}
+      <div id='grupo'>
+        {
+          /* Aqui fica a repetição para mostrar todas as pessoas */
+          pessoas.map((n) => (
+
+            /* Aqui inicia a div das pessoas INDIVIDUALMENTE */
+            <div className='pessoa'>
+
+              {/* Aqui fica a imagem que aparecera para cada pessoa */}
+              <img src={n.imgUrl} alt={`${n.nome} imagem`}/>
+
+              {/* Aqui fica o nome da pessoa que aparecera para cada pessoa */}
+              <h4>{n.nome}</h4>
+
+              {/* Aqui termina a div das pessoas INDIVIDUALMENTE */}
+            </div>
+
+            
+          ))
+        }
+      </div>
+      {/* Aqui termina a div com as pessoas (imagem e nome) EM GRUPO */}
+    </div>
+  );
+}
+
+export default App;
