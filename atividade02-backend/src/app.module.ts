44d2c39e6c8f72b4819a9f65a738475439f24a42
LuizGabe <luizgabrielgrupe@gmail.com>
2022-11-29 17:27:43 -0300

    Configing the database connection

--- atividade02-backend/src/app.module.ts
+++ atividade02-backend/src/app.module.ts
@@ -1,36 +1,34 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { VideoController } from './video.controller';
-import { CategoryController } from './category.controller';
+import { VideoController } from './video/controllers/video/video.controller';
 
-import { Category } from './category.entity';
-import { Video } from './video.entity';
+import { Video } from './typeorm/entities/video';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { VideoService } from './video.service';
-import { CategoryService } from './category.service';
+
+import { VideoModule } from './video/video.module';
+import { VideoService } from './video/services/video/video.service';
 
 @Module({
   imports: [
-    
     TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'senha',
-    database: 'tarefa',
-    entities: [
-      Video,
-      Category // entidades/classes
-    ],
-    synchronize: true
-  }),
-  TypeOrmModule.forFeature([Video, Category])
-
-],
-  controllers: [AppController,VideoController, CategoryController],
-  providers: [AppService, VideoService. CategoryService],
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'senha',
+      database: 'video',
+      entities: [
+        Video // entidades/classes
+      ],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Video]),
+    VideoModule,
+  ],
+  controllers: [AppController, VideoController],
+  providers: [AppService, VideoService],
 })
 export class AppModule {}
