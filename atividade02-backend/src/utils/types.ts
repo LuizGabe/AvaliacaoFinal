9bc46d20c56df3ce12d69b19474cf199d8e9352c
LuizGabe <luizgabrielgrupe@gmail.com>
2022-11-30 18:01:20 -0300

    Criando os tipos de dados aceitos

--- atividade02-backend/src/utils/types.ts
+++ atividade02-backend/src/utils/types.ts
@@ -1,6 +1,12 @@
 /* eslint-disable prettier/prettier */
-export class AddVideoParams {
-  name:string;
+export type AddVideoParams = {
+  name: string;
   url: string;
   id_Category: number;
 }
+
+export type AddCategoryParams = {
+  id: number;
+  name: string;
+}
+
