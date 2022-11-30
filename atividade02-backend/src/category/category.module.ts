7d1c3b100ea043b09e11ed11d01c082b8bd22929
LuizGabe <luizgabrielgrupe@gmail.com>
2022-11-30 17:59:00 -0300

    Criando e configurando as rotas para a entidade Category

--- atividade02-backend/src/category/category.module.ts
+++ atividade02-backend/src/category/category.module.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/typeorm/entities/Category';
 import { CategoryController } from './controllers/category/category.controller';
 import { CategoryService } from './services/category/category.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
   providers: [CategoryService]
 })
